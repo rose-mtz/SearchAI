b34b18c63e535d8f432302d7f69c9ceb3f1bf52d
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-02-28 21:37:53 -0600

    renames

--- src/main.cpp
+++ src/main.cpp
@@ -13,36 +13,40 @@
 #include "SearchAStar.h"
 #include "Renderer.h"
 
+
+// Globals ------------------------------------------------------
+
+
+// Colors
 const glm::vec3 SOLUTION_COLOR = glm::vec3(1.0f, 1.0f, 1.0f);
 const glm::vec3 CLOSED_CELL = glm::vec3(0.725f, 0.024f, 0.035f);
 const glm::vec3 OPEN_CELL = glm::vec3(0.976f, 0.792f, 0.012f);
 const glm::vec3 MOUSE_COLOR = glm::vec3(0.0f, 0.75f, 1.0f);
 
-glm::vec2 mousePos;
+glm::vec2 mouse_pos;
+glm::vec2 path_start;
+glm::vec2 path_end;
+unsigned int selected_path_endpoints = 0;
+bool reinitialize_ai = true;
+SearchAI* path_finder_ai;
+SearchAIType current_ai_type = BFS;
 
-glm::vec2 pathStart;
-glm::vec2 pathEnd;
-unsigned int selectedPathEndpoints = 0;
-
-bool reInitAI = true;
-SearchAI* ai;
-SearchAIType aiType = BFS;
-
-float lastAnimation = 0.0f;
+float last_animation = 0.0f;
 const float ANIMATION_INTERVAL = 0.2f; // seconds
 bool animate = false;
 
 Grid grid;
 
-bool mapCreationMode = false;
-unsigned int selectedCellValue = 0;
+bool map_creation_mode = false;
+unsigned int selected_cell_value = 0;
 const unsigned int COUNT_GRID_DENSITIES = 5;
 const unsigned int GRID_DENSITIES[] = { 16, 32, 64, 128, 256 };
-unsigned int selectedGridDensity = 0;
-unsigned int selectedGridPreMadeMap = 0;
+unsigned int selected_grid_density = 0;
+unsigned int selected_grid_pre_made_map = 0;
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 800;
+
 
 bool initialize(GLFWwindow* &window, unsigned int width, unsigned int height);
 void processInput(GLFWwindow *window);
@@ -53,11 +57,11 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void printGrid();
 
+
 int main()
 {
     // Window
     GLFWwindow* window = nullptr;
-
     if (!initialize(window, SCR_WIDTH, SCR_HEIGHT))
     {
         return -1;
@@ -81,15 +85,15 @@ int main()
         float currentFrame = glfwGetTime();
         processInput(window);
 
-        if (!mapCreationMode)
+        if (!map_creation_mode)
         {
-            if (selectedPathEndpoints == 2 && (ai != nullptr && !ai->done()) && animate && currentFrame + ANIMATION_INTERVAL >= lastAnimation)
+            if (selected_path_endpoints == 2 && (path_finder_ai != nullptr && !path_finder_ai->done()) && animate && currentFrame + ANIMATION_INTERVAL >= last_animation)
             {
-                ai->step();
-                lastAnimation = currentFrame;
+                path_finder_ai->step();
+                last_animation = currentFrame;
             }
 
-            if (reInitAI && selectedPathEndpoints == 2)
+            if (reinitialize_ai && selected_path_endpoints == 2)
             {
                 // ai.init(getCellThatMouseIsOn(grid, pathStart, SCR_WIDTH, SCR_HEIGHT), getCellThatMouseIsOn(grid, pathEnd, SCR_WIDTH, SCR_HEIGHT), &grid, aiType);
                 // if (aiType == DFS)
@@ -101,47 +105,47 @@ int main()
                 //     ai = new SearchBFS(&grid, getCellThatMouseIsOn(grid, pathStart, SCR_WIDTH, SCR_HEIGHT), getCellThatMouseIsOn(grid, pathEnd, SCR_WIDTH, SCR_HEIGHT));
                 // }
                 // ai = new SearchGreedy(&grid, getCellThatMouseIsOn(grid, pathStart, SCR_WIDTH, SCR_HEIGHT), getCellThatMouseIsOn(grid, pathEnd, SCR_WIDTH, SCR_HEIGHT));
-                ai = new SearchAStar(&grid, getCellThatMouseIsOn(grid, pathStart, SCR_WIDTH, SCR_HEIGHT), getCellThatMouseIsOn(grid, pathEnd, SCR_WIDTH, SCR_HEIGHT));
-                reInitAI = false;
+                path_finder_ai = new SearchAStar(&grid, getCellThatMouseIsOn(grid, path_start, SCR_WIDTH, SCR_HEIGHT), getCellThatMouseIsOn(grid, path_end, SCR_WIDTH, SCR_HEIGHT));
+                reinitialize_ai = false;
             }
         }
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        if (!mapCreationMode)
+        if (!map_creation_mode)
         {
             renderer.drawGrid(grid);
 
-            if (selectedPathEndpoints == 2 && ai != nullptr)
+            if (selected_path_endpoints == 2 && path_finder_ai != nullptr)
             {
-                std::vector<Node *> closedList = ai->getClosed();
+                std::vector<Node *> closedList = path_finder_ai->getClosed();
                 for (const Node* aNode : closedList)
                 {
                     renderer.drawCell(aNode->state, grid, CLOSED_CELL);
                 }
-                std::vector<Node*> openList = ai->getOpen();
+                std::vector<Node*> openList = path_finder_ai->getOpen();
                 for (Node* aNode : openList)
                 {
                     renderer.drawCell(aNode->state, grid, OPEN_CELL);
                 }
-                std::vector<Node*> solution = ai->getSolution();
+                std::vector<Node*> solution = path_finder_ai->getSolution();
                 for (Node* aNode : solution)
                 {
                     renderer.drawCell(aNode->state, grid, SOLUTION_COLOR);
                 }
             }
 
-            GridCell mouseCell = getCellThatMouseIsOn(grid, mousePos, SCR_WIDTH, SCR_HEIGHT);
+            GridCell mouseCell = getCellThatMouseIsOn(grid, mouse_pos, SCR_WIDTH, SCR_HEIGHT);
             renderer.drawCell(mouseCell, grid, MOUSE_COLOR);
 
-            GridCell pathStartCell = getCellThatMouseIsOn(grid, pathStart, SCR_WIDTH, SCR_HEIGHT);
-            GridCell pathEndCell = getCellThatMouseIsOn(grid, pathEnd, SCR_WIDTH, SCR_HEIGHT);
-            if (selectedPathEndpoints == 1)
+            GridCell pathStartCell = getCellThatMouseIsOn(grid, path_start, SCR_WIDTH, SCR_HEIGHT);
+            GridCell pathEndCell = getCellThatMouseIsOn(grid, path_end, SCR_WIDTH, SCR_HEIGHT);
+            if (selected_path_endpoints == 1)
             {
                 renderer.drawCell(pathStartCell, grid, SOLUTION_COLOR);
             }
-            else if (selectedPathEndpoints == 2)
+            else if (selected_path_endpoints == 2)
             {
                 renderer.drawCell(pathStartCell, grid, SOLUTION_COLOR);
                 renderer.drawCell(pathEndCell, grid, SOLUTION_COLOR);
@@ -152,8 +156,8 @@ int main()
         else
         {
             renderer.drawGrid(grid);
-            GridCell mouseCell = getCellThatMouseIsOn(grid, mousePos, SCR_WIDTH, SCR_HEIGHT);
-            renderer.drawCell(mouseCell, grid, CELL_COLORS[selectedCellValue]);
+            GridCell mouseCell = getCellThatMouseIsOn(grid, mouse_pos, SCR_WIDTH, SCR_HEIGHT);
+            renderer.drawCell(mouseCell, grid, CELL_COLORS[selected_cell_value]);
             renderer.drawGridLines(grid);
         }
 
@@ -233,10 +237,10 @@ GridCell getCellThatMouseIsOn(Grid& grid,const glm::vec2& mousePos, float gridWi
 
 void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 {
-    mousePos.x = xpos;
-    mousePos.y = ypos;
+    mouse_pos.x = xpos;
+    mouse_pos.y = ypos;
 
-    if (mapCreationMode && glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+    if (map_creation_mode && glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
     {
         const glm::vec2 mousePOS (xpos, ypos); // what a POS mouse!
         GridCell clickedCell = getCellThatMouseIsOn(grid, mousePOS, SCR_WIDTH, SCR_HEIGHT);
@@ -248,7 +252,7 @@ void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
             clickedCell.col < grid.getNumberOfColumns()
         )
         {
-            grid.set(clickedCell.row, clickedCell.col, selectedCellValue);
+            grid.set(clickedCell.row, clickedCell.col, selected_cell_value);
         }
     }
 }
@@ -258,20 +262,20 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
     double xpos, ypos;
     glfwGetCursorPos(window, &xpos, &ypos);
 
-    if (!mapCreationMode)
+    if (!map_creation_mode)
     {
         if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
         {
-            if (selectedPathEndpoints == 0 || selectedPathEndpoints == 2)
+            if (selected_path_endpoints == 0 || selected_path_endpoints == 2)
             {
-                pathStart = glm::vec2(xpos, ypos);
-                selectedPathEndpoints = 1;
+                path_start = glm::vec2(xpos, ypos);
+                selected_path_endpoints = 1;
             }
             else 
             {
-                pathEnd = glm::vec2(xpos,ypos);
-                selectedPathEndpoints = 2;
-                reInitAI = true;
+                path_end = glm::vec2(xpos,ypos);
+                selected_path_endpoints = 2;
+                reinitialize_ai = true;
             }
         }
 
@@ -280,14 +284,14 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
             if (animate)
             {
                 float END_TIME = glfwGetTime() + 5.0f;
-                while (!ai->done() && END_TIME >= glfwGetTime())
+                while (!path_finder_ai->done() && END_TIME >= glfwGetTime())
                 {
-                    ai->step();
+                    path_finder_ai->step();
                 }
             }
             else
             {
-                ai->step();
+                path_finder_ai->step();
             }
         }
     }
@@ -302,7 +306,7 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
             clickedCell.col < grid.getNumberOfColumns()
         )
         {
-            grid.set(clickedCell.row, clickedCell.col, selectedCellValue);
+            grid.set(clickedCell.row, clickedCell.col, selected_cell_value);
         }
     }
 }
@@ -311,21 +315,21 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
     if (key == GLFW_KEY_M && action == GLFW_PRESS)
     {
-        mapCreationMode = !mapCreationMode;
-        if (mapCreationMode)
+        map_creation_mode = !map_creation_mode;
+        if (map_creation_mode)
         {
-            selectedPathEndpoints = 0;
+            selected_path_endpoints = 0;
         }
-        std::cout << "Map creation mode: " << (mapCreationMode ? "ON" : "OFF") << '\n';
+        std::cout << "Map creation mode: " << (map_creation_mode ? "ON" : "OFF") << '\n';
     }
 
-    if (!mapCreationMode)
+    if (!map_creation_mode)
     {
         if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
         {
-            if (ai->done() && selectedPathEndpoints == 2)
+            if (path_finder_ai->done() && selected_path_endpoints == 2)
             {
-                reInitAI = true;
+                reinitialize_ai = true;
                 animate = true;
             }
             else
@@ -335,50 +339,50 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         }
         if (key == GLFW_KEY_ENTER && action == GLFW_PRESS)
         {
-            if (aiType == BFS)
+            if (current_ai_type == BFS)
             {
-                aiType = DFS;
+                current_ai_type = DFS;
                 std::cout << "Search AI: DFS\n";
             }
             else
             {
-                aiType = BFS;
+                current_ai_type = BFS;
                 std::cout << "Search AI: BFS\n";
             }
 
-            reInitAI = true;
+            reinitialize_ai = true;
         }
     }
     else
     {
         if (key == GLFW_KEY_N && action == GLFW_PRESS)
         {
-            selectedCellValue++;
+            selected_cell_value++;
             
-            if (selectedCellValue >= COUNT_CELL_COLORS)
+            if (selected_cell_value >= COUNT_CELL_COLORS)
             {
-                selectedCellValue = 0;
+                selected_cell_value = 0;
             }
         }
         if (key == GLFW_KEY_UP && action == GLFW_PRESS)
         {
-            selectedGridDensity++;
+            selected_grid_density++;
 
-            if (selectedGridDensity >= COUNT_GRID_DENSITIES)
+            if (selected_grid_density >= COUNT_GRID_DENSITIES)
             {
-                selectedGridDensity = COUNT_GRID_DENSITIES - 1;
+                selected_grid_density = COUNT_GRID_DENSITIES - 1;
             }
 
-            grid.changeGrid(GRID_DENSITIES[selectedGridDensity], GRID_DENSITIES[selectedGridDensity]);
+            grid.changeGrid(GRID_DENSITIES[selected_grid_density], GRID_DENSITIES[selected_grid_density]);
         }
         if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
         {
-            if (selectedGridDensity > 0)
+            if (selected_grid_density > 0)
             {
-                selectedGridDensity--;
+                selected_grid_density--;
             }
 
-            grid.changeGrid(GRID_DENSITIES[selectedGridDensity], GRID_DENSITIES[selectedGridDensity]);
+            grid.changeGrid(GRID_DENSITIES[selected_grid_density], GRID_DENSITIES[selected_grid_density]);
         }
         if (key == GLFW_KEY_P && action == GLFW_PRESS)
         {
@@ -386,18 +390,18 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         }
         if (key == GLFW_KEY_C && action == GLFW_PRESS)
         {
-            grid.clear(selectedCellValue);
+            grid.clear(selected_cell_value);
         }
         if (key == GLFW_KEY_B && action == GLFW_PRESS)
         {
-            selectedGridPreMadeMap++;
-            if (selectedGridPreMadeMap >= GRID_RAW_DATA_SIZE)
+            selected_grid_pre_made_map++;
+            if (selected_grid_pre_made_map >= GRID_RAW_DATA_SIZE)
             {
-                selectedGridPreMadeMap = 0;
+                selected_grid_pre_made_map = 0;
             }
 
-            grid.changeGrid(GRID_RAW_DATA[selectedGridPreMadeMap]->rows, GRID_RAW_DATA[selectedGridPreMadeMap]->cols);
-            loadGrid(grid, GRID_RAW_DATA[selectedGridPreMadeMap]->rawData);
+            grid.changeGrid(GRID_RAW_DATA[selected_grid_pre_made_map]->rows, GRID_RAW_DATA[selected_grid_pre_made_map]->cols);
+            loadGrid(grid, GRID_RAW_DATA[selected_grid_pre_made_map]->rawData);
         }
     }
 }
